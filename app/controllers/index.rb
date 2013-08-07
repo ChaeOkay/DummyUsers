b635938397ac2b9332df83bf793cfa8e9854c835
ideahed <apprentice@devbootcamp.com>
2013-08-07 16:42:04 -0700

    make roots

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,18 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/new_user' do
+
+  erb :new_user
+end
+
+post '/new_user' do
+
+  erb :index
+end
+
+get '/secret' do
+  
+  erb :secret
+end
